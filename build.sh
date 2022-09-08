3d9f9edebbc5e8c35e5c45ee1dcbe3f21adc36ce
Fedor Chervyakov <fchervyakov@gmail.com>
2022-09-08 23:13:40 +0300

    build.sh: adapt to armhf target arch

--- build.sh
+++ build.sh
@@ -2,8 +2,8 @@
 
 cd /workspace/src
 mkdir -p /workspace/out
-export CC=aarch64-linux-gnu-gcc
-export $(dpkg-architecture -aarm64); export CROSS_COMPILE=aarch64-linux-gnu-
+export CC=arm-linux-gnueabihf-gcc
+export $(dpkg-architecture -aarmhf); export CROSS_COMPILE=arm-linux-gnueabihf-
 fakeroot debian/rules clean
 fakeroot debian/rules binary-headers binary binary-perarch
 mv ../*.deb ../out/
